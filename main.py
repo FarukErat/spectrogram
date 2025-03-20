cae7c3b93d516134b3e3a0bb3b05354b7d1bd213
FarukErat <farukerat1327@gmail.com>
2025-03-20 22:53:16 +0300

    rename

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from scipy.io import wavfile
 from scipy.signal import spectrogram
 import os
 
-def save_spectrogram(filename, output_file, min_hz=None, max_hz=None,
+def save_spectrogram(audio_file_path, image_file_path, min_hz=None, max_hz=None,
                     min_db=None, max_db=None, dpi=100, format=None):
 
     # Read WAV file
-    sample_rate, data = wavfile.read(filename)
+    sample_rate, data = wavfile.read(audio_file_path)
 
     # Convert stereo to mono
     if len(data.shape) == 2:
         data = data.mean(axis=1)
 
     # Compute spectrogram
-    nperseg = 1024  # Window size
+    nperseg = 2048  # Window size
     noverlap = nperseg // 2  # Overlap between windows
     f, t, Sxx = spectrogram(data, fs=sample_rate, nperseg=nperseg,
                             noverlap=noverlap, window='hann')
@@ -49,11 +49,11 @@ def save_spectrogram(filename, output_file, min_hz=None, max_hz=None,
     cbar = plt.colorbar()
     cbar.set_label('Intensity [dB]')
 
-    plt.title(f"Spectrogram of {os.path.basename(filename)}")
+    plt.title(f"Spectrogram of {os.path.basename(audio_file_path)}")
     plt.tight_layout()
 
     # Save to file instead of displaying
-    plt.savefig(output_file, bbox_inches='tight', dpi=dpi, format=format)
+    plt.savefig(image_file_path, bbox_inches='tight', dpi=dpi, format=format)
     plt.close(fig)
 
 # Example usage:
